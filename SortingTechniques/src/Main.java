54c3eb55a65a68c6251acd83947280a50ad78c17
alninfae <anna.liniu@gmail.com>
2025-06-06 16:27:24 -0400

    more comments

--- SortingTechniques/src/Main.java
+++ SortingTechniques/src/Main.java
@@ -1,3 +1,10 @@
+/**
+ * @author: Anna and Isabella
+ * Comments: Anna: worked on Sort Class and part of View components. Isabella: Worked on controller class, and part of view class
+ * Description: Create an app that tracks the time it takes to sort through 50000 random numbers from least to greates values, using selection sort, bubble sort and insertion Sort
+ * 
+ */
+
 public class Main {
     public static void main(String[] args) {
         ViewApp view = new ViewApp();
